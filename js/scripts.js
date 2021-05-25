bc8121b17a10c7306f4cea9c149c4c512dd842e4
Nassif36 <khalilyoutuber@hotmail.com>
2021-05-25 13:06:18 -0300

    tercer commit scamp

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+'use strict'
+
+window.addEventListener('load', function(){
+
+    var formulario = document.querySelector('#form-contacto');
+
+    formulario.addEventListener('submit', function(){
+        
+
+
+    var enviarForm = document.querySelector('#formEnviar');
+    var errorForm = document.querySelector('#errorForm');
+
+    var numero = 5492291450668;
+    var nombre = document.querySelector('#nombre').value
+    var apellido = document.querySelector('#apellido').value;
+    var mensaje = document.querySelector('#textarea').value;
+    
+            if (nombre.trim() == null || nombre.trim().length == 0) {
+            errorForm.style.display = 'block';
+            errorForm.innerHTML = "Por favor, introducí tu nombre correctamente";
+            return false;
+        } else if (apellido.trim() == null || apellido.trim().length == 0) {
+            errorForm.innerHTML = "Por favor, ntroducí tu apellido correctamente";
+             errorForm.style.display = 'block';
+            return false;
+        }
+        else if (mensaje.trim() == null || mensaje.trim().length == 0){
+            errorForm.style.display = 'block';
+            errorForm.innerHTML = "Escribe tu mensaje";
+            return false;
+        } 
+        else {
+            errorForm.style.display = 'none';
+        }
+        const enviarMensaje = () => {
+            var win = window.open(`https://wa.me/${numero}?text=Hola%20soy%20${nombre}%20${apellido}, Consulta:%20${mensaje}`, '_blank');
+        }
+
+           enviarForm.addEventListener('click', enviarMensaje);
+    });
+
+
+     formulario.event.preventDefault();
+});
